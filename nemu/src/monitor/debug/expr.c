3ff6b70ade1d197d24d03436c122c9a7346c6ba9
tracer-ics2018 <tracer@njuics.org>
2018-12-13 22:06:06 +0800

    >  compile U201514631 wj Linux wj-virtual-machine 4.15.0-42-generic #45-Ubuntu SMP Thu Nov 15 19:32:57 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  22:06:06 up  1:54,  1 user,  load average: 0.05, 0.21, 0.27 a8c6ff9b116adc02658abd01549865cd4505c

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -201,7 +201,6 @@ int eval(int p, int q) {
     }
     int val1 = eval(p, op - 1);
     int val2 = eval(op + 1, q);
-    printf("%d %d %d\n", val1, op, val2);
     if (errexp) {
       return 0;
     }
