4658ed0fd03f2b7182f11fd15d944768b59724f3
tracer-ics2018 <tracer@njuics.org>
2018-12-14 09:50:05 +0800

    >  compile U201514631 wj Linux wj-virtual-machine 4.15.0-42-generic #45-Ubuntu SMP Thu Nov 15 19:32:57 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  09:50:05 up  1:41,  1 user,  load average: 0.23, 0.34, 0.39 5f5cddb9f9751b92cc73449cf7c7a5ea073b70e

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -101,6 +101,7 @@ static bool make_token(char *e) {
           default:
             tokens[nr_token].type = rules[i].token_type;
             strncpy(tokens[nr_token].str, substr_start, substr_len);
+            tokens[nr_token].str[substr_len] = '\0';
             ++nr_token;
             break;
         }
