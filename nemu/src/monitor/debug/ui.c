a0adbaeff746662b68926cc1846ea5457bc743a0
tracer-ics2018 <tracer@njuics.org>
2018-12-15 22:22:05 +0800

    >  compile U201514631 wj Linux wj-virtual-machine 4.15.0-42-generic #45-Ubuntu SMP Thu Nov 15 19:32:57 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  22:22:05 up  5:22,  1 user,  load average: 0.36, 0.30, 0.32 298f409ed7592c8c36409ebecfceccae964ba4d7

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -117,7 +117,8 @@ static int cmd_w(char *args) {
     return 0;
   }
   bool suc = true;
-  while (*arg++ == ' ');
+  while (*arg == ' ')
+    ++arg;
   uint32_t ret = expr(arg, &suc);
   if (suc) {
     WP *wp = new_wp();
