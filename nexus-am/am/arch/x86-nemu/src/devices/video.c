aa317cdd6de6cc3552cb1be96cccd244180a7228
tracer-ics2018 <tracer@njuics.org>
2018-12-20 21:26:56 +0800

    >  run U201514631 wj Linux wj-virtual-machine 4.15.0-42-generic #45-Ubuntu SMP Thu Nov 15 19:32:57 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  21:26:56 up  9:49,  1 user,  load average: 0.31, 0.37, 0.32 a803c97d74fc9729afc764818cbe5062e0de6e4b

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -5,6 +5,10 @@
 
 #define SCREEN_PORT 0x100 // Note that this is not the standard
 
+static inline int min(int x, int y) {
+  return (x < y) ? x : y;
+}
+
 static uint32_t* const fb __attribute__((used)) = (uint32_t *)0x40000;
 
 size_t video_read(uintptr_t reg, void *buf, size_t size) {
@@ -24,9 +28,15 @@ size_t video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _FBCtlReg *ctl = (_FBCtlReg *)buf;
-      int i;
-      int size = screen_width() * screen_height();
-      for (i = 0; i < size; i++)  fb[i] = i;
+      int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+      uint32_t *pixels = ctl->pixels;
+      int W = screen_width();
+      int H = screen_height();
+      int cp_bytes = sizeof(uint32_t) * min(w, W - x);
+      for (int j = 0; j < h && y + j < H; j ++) {
+        memcpy(&fb[(y + j) * W + x], pixels, cp_bytes);
+        pixels += w;
+      }
       if (ctl->sync) {
         // do nothing, hardware syncs.
       }
