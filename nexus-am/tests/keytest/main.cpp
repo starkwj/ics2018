482e38309dda777db5d4697e47b5610f88352d4d
tracer-ics2018 <tracer@njuics.org>
2018-12-20 17:32:18 +0800

    >  run U201514631 wj Linux wj-virtual-machine 4.15.0-42-generic #45-Ubuntu SMP Thu Nov 15 19:32:57 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  17:32:18 up  7:15,  1 user,  load average: 1.35, 0.65, 0.45 8d275ad99f3241a9535b953d4fdabb44fda9f604

--- nexus-am/tests/keytest/main.cpp
+++ nexus-am/tests/keytest/main.cpp
@@ -19,7 +19,6 @@ int main() {
       key ^= 0x8000;
       down = true;
     }
-    printf("k: %d\n", key);
     if (key != _KEY_NONE) {
       printf("Get key: %d %s %s\n", key, names[key], down ? "down" : "up");
     }
