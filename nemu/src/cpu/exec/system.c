9e6f4ee8c8418362626e9c077b154c97e2710770
tracer-ics2018 <tracer@njuics.org>
2018-12-20 08:29:17 +0800

    >  compile U201514631 wj Linux wj-virtual-machine 4.15.0-42-generic #45-Ubuntu SMP Thu Nov 15 19:32:57 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  08:29:17 up 19 min,  1 user,  load average: 0.24, 0.32, 0.28 bf5d86414d88c4bf34f0d6c74df1934a8669a085

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -70,13 +70,13 @@ make_EHelper(out) {
   // TODO();
   switch (id_dest->width) {
     case 4:
-      pio_write_l(id_src->val, id_dest->val);
+      pio_write_l(id_dest->val, id_src->val);
       break;
     case 1:
-      pio_write_b(id_src->val, id_dest->val);
+      pio_write_b(id_dest->val, id_src->val);
       break;
     case 2:
-      pio_write_w(id_src->val, id_dest->val);
+      pio_write_w(id_dest->val, id_src->val);
       break;
     default:
       assert(0);
