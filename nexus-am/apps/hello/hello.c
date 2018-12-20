3f3abd4f595aef4aa36bae6a6214455bb0a90374
tracer-ics2018 <tracer@njuics.org>
2018-12-20 10:51:51 +0800

    >  run U201514631 wj Linux wj-virtual-machine 4.15.0-42-generic #45-Ubuntu SMP Thu Nov 15 19:32:57 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  10:51:51 up  2:41,  1 user,  load average: 0.33, 0.31, 0.28 11c842be5c87579f65055959f333ec02ebf288f9

--- nexus-am/apps/hello/hello.c
+++ nexus-am/apps/hello/hello.c
@@ -1,5 +1,5 @@
 #include <am.h>
-#include <klib.h>
+// #include <klib.h>
 void print(const char *s) {
   for (; *s; s ++) {
     _putc(*s);
@@ -7,9 +7,9 @@ void print(const char *s) {
 }
 int main() {
   for (int i = 0; i < 10; i ++) {
-    // print("Hello World!\n");
+    print("Hello World!\n");
     // printf("Hello World!\n");
-    printf("%s %d\n", "Hello World!", 1234);
+    // printf("%s %d\n", "Hello World!", 1234);
   }
   return 0;
 }
