63aaded5f931275c7acaad5e03b78c3a2298b3ce
tracer-ics2018 <tracer@njuics.org>
2018-12-22 15:42:14 +0800

    >  run U201514631 wj Linux wj-virtual-machine 4.15.0-42-generic #45-Ubuntu SMP Thu Nov 15 19:32:57 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  15:42:14 up 10:10,  3 users,  load average: 0.56, 0.68, 0.48 c085f5ba722956f9aedec3c1d8cce79af76a91d3

--- nexus-am/am/arch/x86-nemu/include/arch.h
+++ nexus-am/am/arch/x86-nemu/include/arch.h
@@ -9,9 +9,13 @@
 typedef uint32_t size_t;
 
 struct _Context {
-  uintptr_t esi, ebx, eax, eip, edx, err, eflags, ecx, cs, esp, edi, ebp;
+  // uintptr_t esi, ebx, eax, eip, edx, err, eflags, ecx, cs, esp, edi, ebp;
+  // struct _Protect *prot;
+  // int       irq;
   struct _Protect *prot;
-  int       irq;
+  uintptr_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
+  int irq;
+  uintptr_t err, eip, cs, eflags;
 };
 
 #define GPR1 eax
--- nexus-am/am/arch/x86-nemu/src/cte.c
+++ nexus-am/am/arch/x86-nemu/src/cte.c
@@ -1,6 +1,6 @@
 #include <am.h>
 #include <x86.h>
-
+#include <klib.h>
 static _Context* (*user_handler)(_Event, _Context*) = NULL;
 
 void vectrap();
@@ -8,6 +8,10 @@ void vecnull();
 
 _Context* irq_handle(_Context *tf) {
   _Context *next = tf;
+  printf("eax: %x ecx: %x esi:%x edi:%x\n", tf->eax, tf->ecx, tf->esi, tf->edi);
+  printf("irq: %x\n", tf->irq);
+  printf("eip: %x cs: %x eflags:%x\n", tf->eip, tf->cs, tf->eflags);
+
   if (user_handler) {
     _Event ev = {0};
     switch (tf->irq) {
