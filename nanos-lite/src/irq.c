4bc189c36c66b10e5abb2bc9c350aaa4585e85ba
tracer-ics2018 <tracer@njuics.org>
2018-12-22 16:34:26 +0800

    >  run U201514631 wj Linux wj-virtual-machine 4.15.0-42-generic #45-Ubuntu SMP Thu Nov 15 19:32:57 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  16:34:26 up 11:02,  3 users,  load average: 0.25, 0.65, 0.66 a784f2a2b5b65cc07cc04e8bbe9e986013312995

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,7 +1,18 @@
 #include "common.h"
 
+// enum {
+//   _EVENT_NULL = 0,
+//   _EVENT_ERROR,
+//   _EVENT_IRQ_TIMER,
+//   _EVENT_IRQ_IODEV,
+//   _EVENT_PAGEFAULT,
+//   _EVENT_YIELD,
+//   _EVENT_SYSCALL,
+// };
+
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
+    case _EVENT_YIELD: printf("Event Yield ID = %d\n", e.event); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
