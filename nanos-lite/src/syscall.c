1476df53d0fc156f93a255611453ab7bef1fd99c
tracer-ics2018 <tracer@njuics.org>
2018-12-22 22:09:21 +0800

    >  run U201514631 wj Linux wj-virtual-machine 4.15.0-42-generic #45-Ubuntu SMP Thu Nov 15 19:32:57 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  22:09:21 up 16:37,  1 user,  load average: 0.43, 0.37, 0.36 7c9af6b43114e478028feba4f16aa735e19222

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,19 @@
 #include "common.h"
 #include "syscall.h"
 
+long sys_write(int fd, const void *buf, size_t count) {
+  if (buf == NULL) {
+    return -1;
+  }
+  int i = 0;
+  if (fd == 1 || fd == 2) {
+    for (; i < count; i++) {
+      _putc(*(const char *)buf);
+    }
+  }
+  return i;
+}
+
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -16,6 +29,9 @@ _Context* do_syscall(_Context *c) {
     case SYS_exit:
       _halt(a[1]);
       c->GPRx = 0;
+      break;
+    case SYS_write:
+      c->GPRx = sys_write(a[1], (const void *)a[2], a[3]);
       break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
