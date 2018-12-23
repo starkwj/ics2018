d0cde9c99b797b719c84e4c96f89f9bf5a67ff31
tracer-ics2018 <tracer@njuics.org>
2018-12-23 16:22:14 +0800

    >  run U201514631 wj Linux wj-virtual-machine 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  16:22:14 up  6:41,  1 user,  load average: 0.50, 0.47, 0.45 67776a929ac70f3cbcbef5aae5237e10e38a580

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -56,12 +56,12 @@ int _read(int fd, void *buf, size_t count) {
 }
 
 int _close(int fd) {
-  _exit(SYS_close);
+  _syscall_(SYS_close, fd, 0, 0);
   return 0;
 }
 
 off_t _lseek(int fd, off_t offset, int whence) {
-  _exit(SYS_lseek);
+  _syscall_(SYS_lseek, fd, offset, whence);
   return 0;
 }
 
