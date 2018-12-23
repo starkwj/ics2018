6924dc20c99a4443d873e622e6b0a95e7f612ec2
tracer-ics2018 <tracer@njuics.org>
2018-12-23 10:26:30 +0800

    >  run U201514631 wj Linux wj-virtual-machine 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  10:26:30 up 46 min,  1 user,  load average: 0.18, 0.27, 0.26 ff641121c1d6f805e53627a66fd060c2219af64d

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -37,7 +37,7 @@ int _write(int fd, void *buf, size_t count){
   return 0;
 }
 
-extern __uint8_t _end;
+extern char _end;
 static intptr_t program_break = (intptr_t)&_end;
 void *_sbrk(intptr_t increment){
   intptr_t old = program_break;
