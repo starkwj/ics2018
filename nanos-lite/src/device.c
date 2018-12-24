3cee95c948731dc49c3d4a1e365f64089d162e79
tracer-ics2018 <tracer@njuics.org>
2018-12-24 13:01:44 +0800

    >  run U201514631 wj Linux wj-virtual-machine 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  13:01:44 up 7 min,  1 user,  load average: 1.16, 0.93, 0.49 a6cc74b3ad2b5f95e00dccab09a9e838841b22b

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -62,8 +62,8 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   //   x = 0;
   //   y++;
   // }
-  printf("fb_write: x=%d len=%d sw=%d\n", x, len, sw);
   if (x + len > sw) {
+    printf("fb_write: x=%d len=%d sw=%d\n", x, len, sw);
     draw = sw - x;
   }
   draw_rect((uint32_t *)(buf + rsz), x, y, draw, 1);
