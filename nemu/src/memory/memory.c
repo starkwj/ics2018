11b2d81dbc8a32053e8fd3466ff2113df4eff9c6
tracer-ics2018 <tracer@njuics.org>
2018-12-26 20:05:13 +0800

    >  compile U201514631 wj Linux wj-virtual-machine 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  20:05:13 up  6:29,  1 user,  load average: 0.41, 0.31, 0.14 c4126baca17fd22966cb828431acef6fece1806

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -52,16 +52,16 @@ void vaddr_write(vaddr_t addr, uint32_t data, int len) {
 paddr_t page_translate(vaddr_t vaddr, bool write) {
   if (cpu.cr0.paging && cpu.cr0.protect_enable) {
     PDE *ppde = (PDE *)(intptr_t)(cpu.cr3.val & ~0xfff);
-    PDE *ppde1 = (PDE *)(intptr_t)(cpu.cr3.val & ~0xfff) + PDX(vaddr);
+    // PDE *ppde1 = (PDE *)(intptr_t)(cpu.cr3.val & ~0xfff) + PDX(vaddr);
     PDE pde;
     pde.val = paddr_read((intptr_t)&ppde[PDX(vaddr)], 4);
-    if (ppde1->val != pde.val) {
-      printf("vaadr=%x   cr3=%x\n", vaddr, cpu.cr3.val);
-      printf("pmem=%lx  guesttohost(0)=%p\n", (intptr_t)pmem, guest_to_host(0));
-      printf("ppde1=%p  &ppde[]=%p\n", ppde1, &ppde[PDX(vaddr)]);
-      printf("pde1=%x pde=%x\n", ppde1->val, pde.val);
-      // assert(0);
-    }
+    // if (ppde1->val != pde.val) {
+    //   printf("vaadr=%x   cr3=%x\n", vaddr, cpu.cr3.val);
+    //   printf("pmem=%lx  guesttohost(0)=%p\n", (intptr_t)pmem, guest_to_host(0));
+    //   printf("ppde1=%p  &ppde[]=%p\n", ppde1, &ppde[PDX(vaddr)]);
+    //   printf("pde1=%x pde=%x\n", ppde1->val, pde.val);
+    //   // assert(0);
+    // }
     assert(pde.present);
     pde.accessed = 1;
     PTE *ppte = (PTE *)(intptr_t)(pde.val & ~0xfff);
