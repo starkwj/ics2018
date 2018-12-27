eff4749b43cf3536075df322093faa07cb8b561a
tracer-ics2018 <tracer@njuics.org>
2018-12-27 20:41:52 +0800

    >  run U201514631 wj Linux wj-virtual-machine 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  20:41:52 up 19 min,  1 user,  load average: 0.32, 0.40, 0.36 3f4d0d968261bc224810b281de967021eda62da

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -23,8 +23,8 @@ extern void context_kload(PCB *pcb, void *entry);
 extern void context_uload(PCB *pcb, const char *filename);
 void init_proc() {
   // naive_uload(NULL, "/bin/init");
-  // context_kload(&pcb[0], (void *)hello_fun);
-  context_uload(&pcb[0], "/bin/hello");
+  context_kload(&pcb[0], (void *)hello_fun);
+  // context_uload(&pcb[0], "/bin/hello");
   // context_uload(&pcb[1], "/bin/init");
   // context_uload(&pcb[0], "/bin/dummy");
   // context_uload(&pcb[0], "/bin/pal");
