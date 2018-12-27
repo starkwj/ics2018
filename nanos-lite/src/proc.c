f4de7458ee3d7dd14ad9d83a210e84dd82ff33be
tracer-ics2018 <tracer@njuics.org>
2018-12-27 21:54:42 +0800

    >  run U201514631 wj Linux wj-virtual-machine 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  21:54:42 up  1:32,  1 user,  load average: 0.38, 0.22, 0.19 5c9e89fd6045b6108d98d0a1d1c124c7e02d5fc4

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -24,9 +24,9 @@ extern void context_uload(PCB *pcb, const char *filename);
 void init_proc() {
   // naive_uload(NULL, "/bin/init");
   // context_kload(&pcb[0], (void *)hello_fun);
-  // context_uload(&pcb[0], "/bin/hello");
-  context_uload(&pcb[0], "/bin/pal");
-  // context_uload(&pcb[1], "/bin/pal");
+  context_uload(&pcb[0], "/bin/hello");
+  // context_uload(&pcb[0], "/bin/pal");
+  context_uload(&pcb[1], "/bin/pal");
   // context_uload(&pcb[0], "/bin/dummy");
   // context_uload(&pcb[0], "/bin/pal");
   switch_boot_pcb();
@@ -34,7 +34,7 @@ void init_proc() {
 
 _Context* schedule(_Context *prev) {
   current->cp = prev;
-  current = &pcb[0];
-  // current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
+  // current = &pcb[0];
+  current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
   return current->cp;
 }
