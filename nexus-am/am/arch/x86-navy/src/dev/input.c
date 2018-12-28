f6da01b4d36a8a4b3e39f925d1d8c6ca78864ea4
tracer-ics2018 <tracer@njuics.org>
2018-12-28 22:55:36 +0800

    >  run U201514631 wj Linux wj-virtual-machine 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  22:55:36 up 17 min,  1 user,  load average: 0.73, 0.81, 0.56 77f3edb61d1ab58bc05d931ae4558986babdb94

--- nexus-am/am/arch/x86-navy/src/dev/input.c
+++ nexus-am/am/arch/x86-navy/src/dev/input.c
@@ -1,15 +1,19 @@
 #include <am.h>
 #include <amdev.h>
 #include <ndl.h>
+#include <klib.h>
 
 size_t input_read(uintptr_t reg, void *buf, size_t size) {
+  printf("input_read:\n");
   switch (reg) {
     case _DEVREG_INPUT_KBD: {
       _KbdReg *kbd = (_KbdReg *)buf;
       NDL_Event e;
+      printf("input test1\n");
       do {
         NDL_WaitEvent(&e);
       } while (e.type != NDL_EVENT_KEYDOWN && e.type != NDL_EVENT_KEYUP);
+      printf("input test2\n");
       kbd->keydown = e.type == NDL_EVENT_KEYDOWN;
       kbd->keycode = e.data;
       return sizeof(_KbdReg);
