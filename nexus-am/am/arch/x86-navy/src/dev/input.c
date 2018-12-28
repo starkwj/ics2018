22078f4dbfef532a284e68495b9dc1d508ca984e
tracer-ics2018 <tracer@njuics.org>
2018-12-28 23:14:21 +0800

    >  run U201514631 wj Linux wj-virtual-machine 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  23:14:21 up 36 min,  1 user,  load average: 0.49, 0.43, 0.44 75cd62328b308a4a9d8489471d41bb090481749

--- nexus-am/am/arch/x86-navy/src/dev/input.c
+++ nexus-am/am/arch/x86-navy/src/dev/input.c
@@ -10,12 +10,15 @@ size_t input_read(uintptr_t reg, void *buf, size_t size) {
       _KbdReg *kbd = (_KbdReg *)buf;
       NDL_Event e;
       printf("input test1\n");
-      do {
-        NDL_WaitEvent(&e);
-      } while (e.type != NDL_EVENT_KEYDOWN && e.type != NDL_EVENT_KEYUP);
+      NDL_WaitEvent(&e);
+      if (e.type != NDL_EVENT_KEYUP && e.type != NDL_EVENT_KEYDOWN) {
+        kbd->keycode = _KEY_NONE;
+        kbd->keydown = false;
+      } else {
+        kbd->keydown = e.type == NDL_EVENT_KEYDOWN;
+        kbd->keycode = e.data;
+      }
       printf("input test2\n");
-      kbd->keydown = e.type == NDL_EVENT_KEYDOWN;
-      kbd->keycode = e.data;
       return sizeof(_KbdReg);
     }
   }
