314012d52ad8d4845e810cb17dea2ba5141090f6
tracer-ics2018 <tracer@njuics.org>
2018-12-28 23:23:59 +0800

    >  run U201514631 wj Linux wj-virtual-machine 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  23:23:59 up 45 min,  1 user,  load average: 0.52, 0.59, 0.52 ee5f88f6f75b20636633c3c7806d420910d3ddc

--- nexus-am/am/arch/x86-navy/src/dev/video.c
+++ nexus-am/am/arch/x86-navy/src/dev/video.c
@@ -30,13 +30,13 @@ size_t video_write(uintptr_t reg, void *buf, size_t size) {
       _FBCtlReg *ctl = (_FBCtlReg *)buf;
       int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
       uint32_t *pixels = ctl->pixels;
-      // int cp_bytes = sizeof(uint32_t) * min(w, W - x);
-      // for (int j = 0; j < h && y + j < H; j ++) {
-      //   memcpy(&fb[(y + j) * W + x], pixels, cp_bytes);
-      //   pixels += w;
-      // }
-      NDL_DrawRect(pixels, x, y, w, h);
-      NDL_Render();
+      int cp_bytes = sizeof(uint32_t) * min(w, W - x);
+      for (int j = 0; j < h && y + j < H; j ++) {
+        memcpy(&fb[(y + j) * W + x], pixels, cp_bytes);
+        pixels += w;
+      }
+      // NDL_DrawRect(pixels, x, y, w, h);
+      // NDL_Render();
       if (ctl->sync) {
         // do nothing, hardware syncs.
       }
