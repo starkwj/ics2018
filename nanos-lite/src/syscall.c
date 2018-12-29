fd7847d49e2b7369170efd9bb7d83d74ffd7b5f9
tracer-ics2018 <tracer@njuics.org>
2018-12-29 16:39:05 +0800

    >  run U201514631 wj Linux wj-virtual-machine 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  16:39:05 up  5:54,  1 user,  load average: 0.40, 0.50, 0.43 53ec0215734ac0cba768e704b813c4e43a0d53e

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -41,8 +41,8 @@ _Context* do_syscall(_Context *c) {
   switch (a[0]) {
     case SYS_exit: {
       // _halt(a[1]);
-      c = sys_execve("/bin/init", NULL, NULL);
-      // memcpy(c, nc, sizeof(_Context));
+      _Context *nc = sys_execve("/bin/init", NULL, NULL);
+      memcpy(c, nc, sizeof(_Context));
       c->GPRx = 0;
       break;
     }
@@ -68,9 +68,9 @@ _Context* do_syscall(_Context *c) {
       c->GPRx = mm_brk(a[1]);
       break;
     case SYS_execve: {
-      c = sys_execve((const char *)a[1], (char *const *)a[2], (char *const *)a[3]);
-      // printf("c=%x  nc=%x\n", c, nc);
-      // memcpy(c, nc, sizeof(_Context));  // important: cover the context
+      _Context *nc = sys_execve((const char *)a[1], (char *const *)a[2], (char *const *)a[3]);
+      printf("c=%x  nc=%x\n", c, nc);
+      memcpy(c, nc, sizeof(_Context));  // important: cover the context
       c->GPRx = 0;
       break;
     }
