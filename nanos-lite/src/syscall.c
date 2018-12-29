5bd63c87b0d2a6c7f83f0f2c1473c8bf9e94b171
tracer-ics2018 <tracer@njuics.org>
2018-12-29 16:09:53 +0800

    >  run U201514631 wj Linux wj-virtual-machine 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  16:09:53 up  5:24,  1 user,  load average: 0.31, 0.24, 0.25 a47f1c37ec7a1b151571ba1e54ff7979f82bbd61

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -23,6 +23,7 @@ long sys_write(int fd, const void *buf, size_t count) {
 
 _Context * sys_execve(const char *filename, char *const argv[], char *const envp[]) {
   // naive_uload(NULL, filename);
+  _vme_init(new_page, free_page);
   reset_page();
   printf("context_uload: %x\n", context_uload);
   context_uload(current, filename);
