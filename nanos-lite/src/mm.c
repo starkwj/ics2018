7080e1cbabb96862214c39279567b681845df18e
tracer-ics2018 <tracer@njuics.org>
2018-12-29 16:02:41 +0800

    >  run U201514631 wj Linux wj-virtual-machine 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  16:02:41 up  5:17,  1 user,  load average: 0.40, 0.34, 0.29 b1b5a3ded3aa9ba33b3e14e3ba0d67dee9bf376b

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -19,8 +19,8 @@ void free_page(void *p) {
 void reset_page() {
   size_t sz = pf - (void *)PGROUNDUP((uintptr_t)_heap.start);
   pf = (void *)PGROUNDUP((uintptr_t)_heap.start);
-  memset(pf, 0, sz);
   printf("reset_page : sz = %x, pf = %x\n", sz, pf);
+  memset(pf, 0, sz);
 }
 
 /* The brk() system call handler. */
